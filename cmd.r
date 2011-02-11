e701738e9a08af043e5096519d9ffa2ba9374a07
Herb Susmann <hps1@lion1.dslab.lan>
2011-02-10 21:54:24 -0500

    Added script to allow creating an animation from the parameters file from the command line

---
+++ cmd.r
@@ -0,0 +1,7 @@
+# Load the core script
+source("/home/hps1/vaccines/vaccinations.r")
+
+# Load default parameters
+source("/home/hps1/vaccines/parameters.r")
+
+source("/home/hps1/vaccines/animate.r")
